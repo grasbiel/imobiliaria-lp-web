5f304f9fb92af6930e48524cac7103a4f56d3147
bzsilva1998 <gabrielbs1998@hotmail.com>
2021-04-06 01:00:19 -0300

    Criação dos primeiros atributos Class Aluguel e Cliente

--- src/main/java/com/imobiliaria/apirest/models/Aluguel.java
+++ src/main/java/com/imobiliaria/apirest/models/Aluguel.java
@@ -1,6 +1,49 @@
 package com.imobiliaria.apirest.models;
 
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 
-public class Aluguel {
+
+@Entity
+@Table(name="tb_aluguel")
+public class Aluguel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+
+    private Date dt_vencimento;
+    private BigDecimal valor_pago;
+    private String obs;
+
+    public Date getDt_vencimento() {
+        return dt_vencimento;
+    }
+
+    public void setDt_vencimento(Date dt_vencimento) {
+        this.dt_vencimento = dt_vencimento;
+    }
+
+    public BigDecimal getValor_pago() {
+        return valor_pago;
+    }
+
+    public void setValor_pago(BigDecimal valor_pago) {
+        this.valor_pago = valor_pago;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
 }
--- src/main/java/com/imobiliaria/apirest/models/Cliente.java
+++ src/main/java/com/imobiliaria/apirest/models/Cliente.java
@@ -1,4 +1,19 @@
 package com.imobiliaria.apirest.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="tb_cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue ( strategy= GenerationType.AUTO)
+    private  int id;
+
+    private String nome_cliente;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private Date dt_nascimento;
 }
